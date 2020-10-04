4819c7b75e04b2e70dfce6785b7d27f8ed300ed2
bitchigo <1020142782@qq.com>
2020-10-04 14:01:16 +0800

    2020/10/4

--- 134.加油站.java
+++ 134.加油站.java
@@ -9,10 +9,10 @@ class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int all=0,sub=0,start=0;
         for(int i=0;i<gas.length;i++){
-            sub+=gas[i]-cost[i];
-            all+=gas[i]-cost[i];
+            sub+=gas[i]-cost[i];       //计算局部路线
+            all+=gas[i]-cost[i];      //计算总油数
             if(sub<0){
-                start=i+1;
+                start=i+1;     //换下一个起点  此处不通前面大概率不通
                 sub=0;
             }
         }
